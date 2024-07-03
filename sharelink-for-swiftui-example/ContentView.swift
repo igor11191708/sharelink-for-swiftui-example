2f9c451314eb0ff3fbc66f408f3e010a8e110233
Igor <igorshelopaev@gmail.com>
2024-07-03 09:17:49 +0200

    Initial Commit

---
+++ sharelink-for-swiftui-example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sharelink-for-swiftui-example
+//
+//  Created by Igor  on 03.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sharelink-for-swiftui-example/sharelink_for_swiftui_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sharelink_for_swiftui_exampleApp.swift
+//  sharelink-for-swiftui-example
+//
+//  Created by Igor  on 03.07.24.
+//
+
+import SwiftUI
+
+@main
+struct sharelink_for_swiftui_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
